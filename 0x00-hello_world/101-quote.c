3e5cdbc28ee24b6e30f9a1a79c425faf27800e58
tgpaulos <warsidotigist11@gmail.com>
2023-05-19 13:45:42 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - \
-			Dora Korpar, \
-			2015-10-19\n");
+	char *message = "and that piece of art is useful\" -Dora Korpar,2015-10-19\n";
+
+	write(2, message, 59);
 	return (1);
 }
