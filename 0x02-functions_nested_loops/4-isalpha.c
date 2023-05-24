e0003e2e7defdb3652944919cb69fb34c0979970
tgpaulos <warsidotigist11@gmail.com>
2023-05-24 01:43:49 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha -checks for l
+ * @c : is the charater to be checked
+ * Return: 1 for alphabet, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
