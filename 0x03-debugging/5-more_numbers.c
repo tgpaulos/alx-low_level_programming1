14ab02ffa95c7b953b19f338c022a22bddd085c3
tgpaulos <warsidotigist11@gmail.com>
2023-05-28 13:37:52 -0700

    print numbers

---
+++ 0x03-debugging/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - This program prints 10 times the numbers, from 0 to 14
+ * 
+ * Return: 0(success) 
+ */
+
+
+void more_numbers( )
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
