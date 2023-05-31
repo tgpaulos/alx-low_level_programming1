3d6390bbbde8c83511e7af8aa54d6ec805948485
tgpaulos <warsidotigist11@gmail.com>
2023-05-31 14:31:22 -0700

    print revers string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,26 +3,25 @@
 /**
  * print_rev - print astring in reverse
  * @s:string
- * @len: length of string
  * Return:0(success)
  */
 void print_rev(char *s)
 {
-    int i; 
-    int len = 0;
-    
+	int i;
+	int len = 0;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-    
-    
-    for (i = len - 1; i >= 0; i--)
-    {
-        putchar(s[i]);
-    }
-    
 
-    putchar('\n');
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+
+
+	putchar('\n');
 }
