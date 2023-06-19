ace5f67912dcb6678219cea67962af4a8a64cbfd
tgpaulos <warsidotigist11@gmail.com>
2023-06-19 10:06:26 -0700

    modified

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("This program was compiled from the file: %s\n", __FILE__);
-    return 0;
+/**
+ * main - This program prints the name of the file it was compiled from
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	printf("This program was compiled from the file: %s\n", __FILE__);
+	return (0);
 }
