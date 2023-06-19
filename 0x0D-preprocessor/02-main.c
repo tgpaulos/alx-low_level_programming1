8a38475817fa6b6720eb71beeb30ccd13ad2ea9e
tgpaulos <warsidotigist11@gmail.com>
2023-06-19 10:18:27 -0700

    file name

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-        printf("This program was compiled from the file: %s\n", __FILE__);
-        return (0);
+	printf("This program was compiled from the file: %s\n", __FILE__);
+	return (0);
 }
