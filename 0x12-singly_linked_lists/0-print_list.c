f3879f3e3e132d359437e6fcfe77a7a8a52b0544
tgpaulos <warsidotigist11@gmail.com>
2023-06-30 15:34:37 -0700

    updates

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,23 +9,20 @@
  *Return: count
  */
 
-
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-
-	if (h == NULL)
-	{
-		printf("[0] (nil)\n");
-		return (count);
-	}
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str);
-		count++;
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		nodes++;
 		h = h->next;
 	}
 
-	return (count);
+	return (nodes);
 }
