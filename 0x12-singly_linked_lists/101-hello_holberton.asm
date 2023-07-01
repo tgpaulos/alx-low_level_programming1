f51dd632423adae0e5b218449727c8f488b30e52
tgpaulos <warsidotigist11@gmail.com>
2023-07-01 10:47:39 -0700

    hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,14 @@
+global    main
+extern    printf
+
 section .data
-    hello db "Hello, Holberton", 10  ; 10 represents the newline character
-    format db "%s", 0
+    format db "Hello, Holberton\n", 0
 
 section .text
-    extern printf
-
-global main
 main:
-    sub rsp, 8      ; align the stack
+    mov   edi, format
+    xor   eax, eax
+    call  printf
+    mov   eax, 0
+    ret
 
-    mov rdi, format ; address of the format string
-    mov rsi, hello  ; address of the hello string
-    xor eax, eax    ; clear EAX register
-    call printf     ; call printf function
-
-    add rsp, 8      ; restore the stack
-    xor eax, eax    ; clear EAX register
-    ret             ; return from the program
