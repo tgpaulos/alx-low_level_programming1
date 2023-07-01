0a3a1ad19e5072f9d9eaba45a610bdec34387231
tgpaulos <warsidotigist11@gmail.com>
2023-07-01 10:49:04 -0700

    hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,9 @@
 global    main
-extern    printf
-
-section .data
-    format db "Hello, Holberton\n", 0
-
-section .text
+          extern    printf
 main:
-    mov   edi, format
-    xor   eax, eax
-    call  printf
-    mov   eax, 0
-    ret
-
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
