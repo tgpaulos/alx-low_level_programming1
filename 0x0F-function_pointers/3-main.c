ba59aef22b3b47028f904c27e7c4cf42d76e9eca
tgpaulos <warsidotigist11@gmail.com>
2023-07-02 18:39:52 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,36 +6,38 @@
  * main - Prints the result of simple operations
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments
+ *
  * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int x, y;
-	int (*o)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	o = get_op_func(argv[2]);
-	if (o == NULL)
+	if ((*op == '/' && num2 == 0) ||
+		(*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
-
-	x = atoi(argv[1]);
-	y = atoi(argv[3]);
-
-	printf("%d\n", o(x, y));
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
