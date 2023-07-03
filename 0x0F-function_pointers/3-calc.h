c085683dc7cf3e07789339ade2df8a6fed0734f0
tgpaulos <warsidotigist11@gmail.com>
2023-07-02 18:22:42 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,16 +1,16 @@
 #ifndef CALC_H
 #define CALC_H
 /**
- * struct op - Struct op
- *
+ * struct op - Struct operation
  * @op: The operator
  * @f: The function associated
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
-} op_t;
+char *op;
+int (*f)(int a, int b);
+}
+op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,7 +5,7 @@
 /**
  * main - Prints the result of simple operations
  * @argc: The number of arguments supplied to the program.
- * @*argv: An array of pointers to the arguments
+ * @argv: An array of pointers to the arguments
  * Return: Always 0.
  */
 
