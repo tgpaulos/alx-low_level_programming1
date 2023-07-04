cf690c01b09468040fda7c906a59a4c4a93ea20d
tgpaulos <warsidotigist11@gmail.com>
2023-07-04 07:46:18 -0700

    updated print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,18 +5,6 @@
  * Return: length of nodes
  */
 
-/*size_t print_listint(const listint_t *h)
-{
-	size_t count = 0;
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-
-	return (count);
-}*/
 size_t print_listint(const listint_t *h)
 {
 	size_t num;
